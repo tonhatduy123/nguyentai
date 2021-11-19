c3afacd4daf9a988f51296ac5657e220be9c14d3
To Nhat Duy <duytngcc19188@fpt.edu.vn>
2021-11-19 12:29:21 +0700

    p

--- Shopping/connection.php
+++ Shopping/connection.php
@@ -1,5 +1,6 @@
 <?php
-    $conn = pg_connect("postgres://xtbolcttgoxqfm:03f3b9493f05123deb10310048637d0c42e2ee7fd54c4c6ce391e3efe9800c5b@ec2-54-211-77-238.compute-1.amazonaws.com:5432/d1ad4q35f1e5tb");
+    $conn = pg_connect("postgres://ajvzlgenarbcjw:d5e2a96868f7a4dc83dbb0dc7c6198890dc2839123bb8ae31bf8fed97dd75ccb@ec2-3-208-157-78.compute-1.amazonaws.com:5432/d9gftdph802ohn");
+
     if(!$conn){
         die("Can not connect database");
     } 
